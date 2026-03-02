00093f2809511be6967c20d83b1ee3d965bffd10
Ankush Gupta <Ankushar143@gmail.com>
2026-03-03 02:28:53 +0530

    navbar name changes

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import './Navbar.css'
+import './navbar.css'
 import { useState } from 'react'
 import { API } from './api'
 
